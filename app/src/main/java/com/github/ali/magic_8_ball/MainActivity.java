6944cd46f86c36463c888c5637df94207560f1eb
Ali Baba <alibabatorjan001@gmail.com>
2019-04-10 13:02:49 +0500

    Initial commit

---
+++ app/src/main/java/com/github/ali/magic_8_ball/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.ali.magic_8_ball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
